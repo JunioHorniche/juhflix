07a5605e66a13f85dd0da958af087df5e1479e27
Junio Horniche <juniohorniche@gmail.com>
2020-07-29 00:41:31 -0300

    Implementação dos componentes gerais/Pag Cadastros

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+import { 
+  BrowserRouter,
+  Switch,
+  Route
+ } from 'react-router-dom';
+import Home from './pages/Home';
+import CasdastroVideo from './pages/cadastro/Video'
+import CasdastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio master blaster na descrição
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CasdastroVideo} exact/>
+      <Route path="/cadastro/categoria" component={CasdastroCategoria} exact/>
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
